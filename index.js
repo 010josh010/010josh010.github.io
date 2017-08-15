c3d8cd7df1d22bc370410997293a8448d1cef722
Joshua Vaughn <Joshua_Vaughn@MacBook-Pro-4.local>
2017-08-15 19:50:59 -0400

    modified detectIE function

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 //getting the project bin to place the projects into 
 var projectBin = document.getElementById('project-bin'); 
 
-var isIE = function(){
-    if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1)){
-        console.log('you are using ie'); 
-    } else {
-        console.log('you are not using ie');
-    }
+
+// returns version of IE or false, if browser is not Internet Explorer
+var detectIE = function() {
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+  // other browser
+  return false;
 }
 
+// Get IE or Edge browser version
+var version = detectIE();
+
+if (version === false) {
+  console.log('you are using a modern browser')
+} else if (version >= 12) {
+  console.log('you are using edge or ie12')
+} else {
+  console.log('you are using ie 11 and below')
+}
 
 
 //ajax promise call for projects
